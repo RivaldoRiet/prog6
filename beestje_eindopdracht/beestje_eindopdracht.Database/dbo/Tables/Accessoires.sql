6c37d7b2e874da946465f7dc5265d1618df4eec8
Rivaldo Sichtman <rw.sichtman@student.avans.nl>
2019-12-21 17:26:18 +0100

    EF ADDED

---
+++ beestje_eindopdracht/beestje_eindopdracht.Database/dbo/Tables/Accessoires.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[Accessoires](
+	[Id] [int] NOT NULL,
+	[Naam] [nvarchar](max) NOT NULL,
+	[Prijs] [money] NOT NULL,
+	[Afbeelding] [nvarchar](max) NOT NULL,
+	[Beestje_id] [int] NOT NULL,
+ CONSTRAINT [PK_Accessoires] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Accessoires]  WITH CHECK ADD  CONSTRAINT [FK_Accessoires_Beestjes] FOREIGN KEY([Beestje_id])
+REFERENCES [dbo].[Beestjes] ([Id])
+GO
+
+ALTER TABLE [dbo].[Accessoires] CHECK CONSTRAINT [FK_Accessoires_Beestjes]
