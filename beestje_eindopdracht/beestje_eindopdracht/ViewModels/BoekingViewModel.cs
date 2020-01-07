61bc164944950e38e3315ea2c8844513b8a438c2
LucasMakvanWaay <lfmmakva@avans.nl>
2020-01-07 22:27:41 +0100

    Update BoekingViewModel.cs

--- beestje_eindopdracht/beestje_eindopdracht/ViewModels/BoekingViewModel.cs
+++ beestje_eindopdracht/beestje_eindopdracht/ViewModels/BoekingViewModel.cs
@@ -12,33 +12,35 @@ namespace beestje_eindopdracht.ViewModels
     public class BoekingViewModel
     {
         public int id { get; set; }
-
+        [Display(Name = "Contact adres:")]
         [Required(ErrorMessage = "Adres is niet geldig")]
         [MinLength(2)]
         [MaxLength(20)]
         public string contact_adres { get; set; }
-        [Display(Name = "Email address")]
-        [Required(ErrorMessage = "Email adress niet valide")]
-        [EmailAddress(ErrorMessage = "Email adress niet valide")]
+        [Display(Name = "Emailaddress")]
+        [EmailAddress(ErrorMessage = "Email adress is niet valide")]
         public string contact_email { get; set; }
-        [Required(ErrorMessage = "Adres is niet valide")]
+        [Display(Name = "Contact naam:")]
+        [Required(ErrorMessage = "Contact naam is niet valide")]
         [MinLength(2)]
         [MaxLength(20)]
         public string contact_naam { get; set; }
-        [Display(Name = "Je telefoon nummer")]
-        [Required(ErrorMessage = "Een telefoonnummer is verreist.")]
+        [Display(Name = "Telefoonnummer:")]
         [DataType(DataType.PhoneNumber, ErrorMessage = "Telefoon nummer niet valide")]
         [RegularExpression(@"^([0-9]{10})$", ErrorMessage = "Telefoon nummer niet valide")]
         public string contact_telefoonnummer { get; set; }
 
         public DateTime dateTime { get; set; }
+        public ICollection<Beestjes> beestjes { get; set; }
         public IEnumerable<Beestjes> UnavailableBeestjes { get; set; }
         public IEnumerable<Beestjes> AvailableBeestjes { get; set; }
         public IEnumerable<Beestjes> SelectedBeestjes { get; set; }
+        public IEnumerable<Accessoires> SelectedAccessoires { get; set; }
 
         public BoekingViewModel(Boeking boeking)
         {
             id = boeking.Id;
+            beestjes = boeking.Beestjes;
             contact_adres = boeking.contact_adres;
             contact_email = boeking.contact_email;
             contact_naam = boeking.contact_naam;
@@ -50,29 +52,17 @@ namespace beestje_eindopdracht.ViewModels
             UnavailableBeestjes = new List<Beestjes>();
             AvailableBeestjes = new List<Beestjes>();
             SelectedBeestjes = new List<Beestjes>();
+            SelectedAccessoires = new List<Accessoires>();
         }
 
         public BoekingViewModel(IEnumerable<Beestjes> unavailableBeestjes,
-                        IEnumerable<Beestjes> availableBeestjes, IEnumerable<Beestjes> selectedBeestjes, DateTime date)
+                        IEnumerable<Beestjes> availableBeestjes, IEnumerable<Beestjes> selectedBeestjes, DateTime date, IEnumerable<Accessoires> selectedAccessoires)
         {
             UnavailableBeestjes = unavailableBeestjes;
             AvailableBeestjes = availableBeestjes;
             SelectedBeestjes = selectedBeestjes;
             dateTime = date;
-        }
-
-        public Boeking ToNewInstanceModel()
-        {
-            return new Boeking()
-            {
-                Id = this.id,
-                Beestjes = this.SelectedBeestjes.ToList(),
-                datum = this.dateTime,
-                contact_adres = this.contact_adres,
-                contact_email = this.contact_email,
-                contact_naam = this.contact_naam,
-                contact_telefoonnummer = this.contact_telefoonnummer
-        };
+            SelectedAccessoires = selectedAccessoires;
         }
     }
 }
